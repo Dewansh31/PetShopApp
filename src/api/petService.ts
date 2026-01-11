840860d1a85a38bf096c63f6850edea923ee8c11
Dewansh Yadav <dewanshyadaw8@gmail.com>
2026-01-11 11:08:05 +0530

    Refactor code structure for improved readability and maintainability

--- src/api/petService.ts
+++ src/api/petService.ts
@@ -4,12 +4,16 @@ const API_BASE_URL = 'https://reqres.in/api';
 const DOG_API_URL = 'https://dog.ceo/api/breeds/image/random';
 
 export const submitPetDetails = async (petData: any) => {
+
+  return ; // return for now as this is a mock API
+
   try {
     const response = await axios.post(`${API_BASE_URL}/users`, petData);
     return response.data;
   } catch (error) {
     throw new Error('Failed to submit pet details');
   }
+  
 };
 
 export const fetchRandomDogImage = async () => {
